0bde8ccbf63267192893ccebfbf8f0f560b6e1cd
Ismael Jiménez <87533490+rcnlvr@users.noreply.github.com>
2024-11-18 22:38:10 -0600

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -112,7 +112,6 @@ start_date_options = {
     "3 meses": end_date - timedelta(days=91),
     "6 meses": end_date - timedelta(days=182),
     "1 año": end_date - timedelta(days=365),
-    "2 años": end_date - timedelta(days=2*365)
 }
 selected_window = st.sidebar.selectbox("Seleccione la ventana de tiempo para el análisis:", list(start_date_options.keys()))
 start_date = start_date_options[selected_window]
@@ -220,7 +219,7 @@ else:
 
         # Rendimientos y métricas de riesgo en diferentes ventanas de tiempo
         st.subheader("Rendimientos y Métricas de Riesgo en Diferentes Ventanas de Tiempo")
-        ventanas = [30, 90, 180, 350]
+        ventanas = [30, 90, 180, 272]
         
         # Crear DataFrames separados para cada métrica
         rendimientos_ventanas = pd.DataFrame(index=['Portafolio'] + simbolos + [selected_benchmark])
