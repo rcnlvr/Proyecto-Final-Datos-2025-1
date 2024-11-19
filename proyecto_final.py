5bbc3680c14d05905839d462807e3fbea59ce603
Ismael Jiménez <87533490+rcnlvr@users.noreply.github.com>
2024-11-19 01:16:48 -0600

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -35,19 +35,12 @@ def calcular_var_ventana(returns, window):
         return np.nan
     window_returns = returns.iloc[-window:]
     return calcular_var(window_returns)
-
-# Función para calcular VaR usando simulación de Monte Carlo
-def calcular_var_montecarlo(normalized_prices, confidence_level=0.95, num_simulations=1000000):
-    simulated_returns = np.random.choice(normalized_prices, size=(num_simulations, len(normalized_prices)))
-    portfolio_returns = np.sum(simulated_returns, axis=1)
-    var_montecarlo = (np.percentile(portfolio_returns, (1 - confidence_level)*100))
-    return var_montecarlo
     
-def var_montecarlo_ventana(returns, window):
-    if len(returns) < window:
-        return np.nan
-    window_returns = returns.iloc[-window:]
-    return calcular_var_montecarlo(window_returns)
+#def var_montecarlo_ventana(returns, window):
+#    if len(returns) < window:
+#        return np.nan
+#    window_returns = returns.iloc[-window:]
+#    return calcular_var_montecarlo(window_returns)
 
 def crear_histograma_distribucion(returns, var_95, title):
     # Crear el histograma base
